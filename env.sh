64b90d9c67b6ca2c941378441c2c1fc778ef9b51
John Fulton <fulton@redhat.com>
2021-10-27 14:10:04 +0000

    add sleep/retry to bootstrap

--- env.sh
+++ env.sh
@@ -14,9 +14,18 @@ source /opt/directord/bin/activate
 
 directord manage --list-nodes
 if [[ $? -gt 0 ]]; then
-    echo "ERROR: no nodes"
-    echo "directord manage --list-nodes"
-    exit 1
+    echo "WARNNING: no nodes (sleep/retry)"
+    sleep 30
+    pushd env/directord/
+    bash bootstrap.sh
+    bash chmod.sh
+    popd
+    directord manage --list-nodes
+    if [[ $? -gt 0 ]]; then
+        echo "ERROR: no nodes"
+        echo "directord manage --list-nodes"
+        exit 1
+    fi
 fi
 
 # task-core
